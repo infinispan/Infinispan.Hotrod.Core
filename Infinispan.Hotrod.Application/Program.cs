9f056e9c51c910410fb46b5abd30fb83304161fc
Vittorio Rigamonti <vrigamon@redhat.com>
2021-05-20 16:29:07 +0200

    Application example

---
+++ Infinispan.Hotrod.Application/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Infinispan.Hotrod.Core;
+namespace Infinispan.Hotrod.Application
+{
+
+    class Program
+    {
+        static string checkValue = "";
+        static async Task Main(string[] args)
+        {
+            var myGreetings = "Hello World!";
+            Console.WriteLine(myGreetings);
+            InfinispanDG dg = DefaultInfinispan.Instance;
+            // Use a non-authenticated non-encrypted cluster;
+            dg.AddHost("127.0.0.1",11222, false);
+            var cache = dg.newCache<string,string>(new StringMarshaller(), new StringMarshaller(), "default");
+            await cache.Put("greetings",myGreetings);
+            string result = await cache.Get("greetings");
+            Console.WriteLine("Greetings from cache: "+result);
+        }
+    }
+}
