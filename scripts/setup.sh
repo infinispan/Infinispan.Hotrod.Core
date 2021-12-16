9cacc74fbcdf3453994d4063098d6b4914cb27b4
Vittorio Rigamonti <vrigamon@redhat.com>
2021-12-16 21:34:20 +0100

    moved

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ if [ -z $ISPN_HOME ]; then
     ISPN_VER=${ISPN_VER:='13.0.3.Final'}
     wget -q -N https://downloads.jboss.org/infinispan/$ISPN_VER/infinispan-server-$ISPN_VER.zip
     unzip -n infinispan-server-$ISPN_VER.zip
-    ISPN_HOME=$(echo $PROJ_ROOT/demo/Query/infinispan-server-$ISPN_VER)
+    ISPN_HOME=$(echo $PROJ_ROOT/infinispan-server-$ISPN_VER)
     echo "ISPN_HOME=$ISPN_HOME"
 fi
 cp -r ./conf/* $ISPN_HOME/server/conf
